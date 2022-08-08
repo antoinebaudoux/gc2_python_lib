b8fbce7d60d22fff3e17e3ed1211e417ee619965
Antoine <abaudoux@gmail.com>
2022-08-08 20:45:14 +0200

    initial setup

--- gc2_lib/gc2.py
+++ gc2_lib/gc2.py
@@ -2,8 +2,7 @@
 import requests
 
 
-def update_lesson_status():
-    """This program adds two
-    numbers and return the result"""
-    response = requests.post('https://hook.eu1.make.com/lw29fl3xmh32z7975lmhrroceywmvrw2', data={'ex_1': 'YES'})
+def update_lesson_status(user, lesson, progress):
+    data = {'user': user, 'lesson': lesson, 'progress': progress}
+    response = requests.post('https://hook.eu1.make.com/lw29fl3xmh32z7975lmhrroceywmvrw2', data=data)
     print(response)
