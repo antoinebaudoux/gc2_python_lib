40c8177e0775b078b92cef6d2e3e14913a5fbad1
Antoine <abaudoux@gmail.com>
2022-08-08 21:41:13 +0200

    initial setup

--- gc2_lib/gc2.py
+++ gc2_lib/gc2.py
@@ -2,7 +2,7 @@
 import requests
 
 
-def update_lesson_status(user, lesson, progress):
-    data = {'user': user, 'lesson': lesson, 'progress': progress}
+def update_lesson_status(lesson, user, progress):
+    data = {'lesson': lesson, 'user': user, 'progress': progress}
     response = requests.post('https://hook.eu1.make.com/lw29fl3xmh32z7975lmhrroceywmvrw2', data=data)
     print(response)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gc2_lib",
-    version="0.1.4",
+    version="0.1.5",
     author="Antoine Baudoux",
     author_email="abaudoux@gmail.com",
     packages=find_packages(include=['gc2_lib', 'gc2_lib.*']),
